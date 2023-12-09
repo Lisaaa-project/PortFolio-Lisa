79b85d1c0d93d472304cff520b7f9d5c31b158a0
Kevin <116793967+KevinML48@users.noreply.github.com>
2023-12-10 00:24:44 +0100

    project, night color, img, text

--- script.js
+++ script.js
@@ -2,10 +2,12 @@ let nightMode = false;
 
 const button = document.getElementById('button');
 const text = document.getElementById('nightMode');
+const projectsSection = document.getElementById('projects'); // Ajout de cette ligne
 
 button.addEventListener('click', () => {
-  nightMode = !nightMode;
-  document.body.className = nightMode ? "night-mode" : "";
-  button.className = nightMode ? "button on" : "button";
-  text.innerText = nightMode ? "on" : "off";
+    nightMode = !nightMode;
+    document.body.className = nightMode ? "night-mode" : "";
+    button.className = nightMode ? "button on" : "button";
+    text.innerText = nightMode ? "on" : "off";
+    projectsSection.className = nightMode ? "projects night-mode" : "projects"; // Ajout de cette ligne
 });
